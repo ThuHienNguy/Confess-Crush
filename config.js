3edecd10cfb38267b0e5fd96bbc84f1b5324097c
ThuHienNguy <94470658+ThuHienNguy@users.noreply.github.com>
2021-11-18 18:03:36 +0700

    Update config.js

--- config.js
+++ config.js
@@ -4,7 +4,7 @@ const CONFIG = {
     introDesc: ' Chúc cạu, người tớ yêu sẽ luôn vui vẻ, mạnh khỏe, hạnh phúc
     Luôn tự tin vững bước trên con đường đã chọn
     Nếu mệt mỏi, hãy ngoảnh lại phía sau, sẽ có tớ, luôn bên cạu, cùng cậu vượt qua mỏi thử thách, cùng cạu sẻ chia những buồn vui hay đau khổ. Dù có chuyện gì xảy ra, tớ mong mình sẽ là người được biết chuyện để được giúp đỡ cạu, có chuyện gì hãy nói với tớ nhé <3
-    Luv u forever <3`,
+    Luv u forever <3',
     btnIntro: '^^HiHi^^',
     title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 🥰',
     desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
